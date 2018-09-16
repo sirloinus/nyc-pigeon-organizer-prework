e3a4d288667faa2ff74e28824c43fa8c162e56c3
Amalie S <amaliesmidth@gmail.com>
2018-09-16 12:06:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,9 +5,9 @@ def nyc_pigeon_organizer(data)
   data.each do |data_type, attributes|  # data_type(color/gender/lives) attributes(purple+ etc. / male+ etc. / subway+ etc.)
     attributes.each do |attribute, names|  # attribute (purple etc. / male etc./ subway etc.)  name (Theo etc.)
       names.each do |name|
-        sorted_pigeons[name] ||= {}
-        sorted_pigeons[name][data_type] ||= []
-        sorted_pigeons[name][data_type] << attribute.to_s
+        sorted_pigeons[name] ||= {}     #||= is checking whether left h-s is truthy (exists), if not it adds hash, if yes ||= ensures it is not overwritten
+        sorted_pigeons[name][data_type] ||= []   # same again but adding an array
+        sorted_pigeons[name][data_type] << attribute.to_s     
       end
     end
   end
